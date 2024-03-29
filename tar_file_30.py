d6c4bf8513e41b02eb4c45a2b3fb2985e7c4a3d8
Ahmed Ghazouani <nibniw@gmail.com>
2024-03-29 02:38:01 +0100

    Update tar_file_30.py

--- tar_file_30.py
+++ tar_file_30.py
@@ -1,43 +1,38 @@
 import os
 import subprocess
-import sys
-import time
 import datetime
+import sys
+
+def create_tar_file_name(directory, extension):
+    current_date = datetime.datetime.now().strftime("%d_%m_%Y")
+    base_name = os.path.basename(directory)
+    return f"{base_name}_{current_date}{extension}.tar"
 
 def tar_files_with_extension(directory, extension, destination_folder):
-    date_for_tar = datetime.datetime.now()
-    final_date_for_tar = date_for_tar.strftime("%d_%m_%Y")
-
-    tar_file_name = os.path.basename(directory) + '_' + final_date_for_tar + extension + '.tar'
+    tar_file_name = create_tar_file_name(directory, extension)
     tar_path = os.path.join(destination_folder, tar_file_name)
-
     tar_command = ['tar', '-cf', tar_path]
 
     os.chdir(directory)  # Change the working directory to the given directory
-    list_to_delete = []
-    
 
-    for file in os.listdir('.'):
-    	
-        file_path = os.path.join(directory, file)
-
-        if file.endswith(extension):
-
-        #if os.path.isfile(file_path) and file.endswith(extension) and (time.time() - os.path.getmtime(file_path)) > 30 * 24 * 60 * 60:
-            tar_command.append(file)
-            list_to_delete.append(file)
+    files_to_tar = [file for file in os.listdir('.') if file.endswith(extension)]
+    tar_command.extend(files_to_tar)
 
     subprocess.call(tar_command)
-    for files in list_to_delete:
-       os.remove(files)
 
+    for file in files_to_tar:
+        os.remove(file)
 
-if len(sys.argv) < 3:
-    print("Please provide directory path and file extension as command-line arguments.")
-    sys.exit(1)
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: <script> <directory_path> <extension>")
+        sys.exit(1)
 
-directory_path = sys.argv[1]
-extension = sys.argv[2]
-destination_folder = '/home/kali/Desktop/testing/move'
+    directory_path = sys.argv[1]
+    extension = sys.argv[2]
+    destination_folder = '/home/kali/Desktop/testing/move'
 
-tar_files_with_extension(directory_path, extension, destination_folder)
+    tar_files_with_extension(directory_path, extension, destination_folder)
+
+if __name__ == "__main__":
+    main()
